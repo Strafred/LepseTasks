48cb3c4ddc03ba886be0cf79916181ea16fb2a7e
Strafred <bred7355608@gmail.com>
2024-06-10 23:35:09 +0300

    fix task 4

--- src/main/java/lepse/BoxTask/BoxTaskSolver.java
+++ src/main/java/lepse/BoxTask/BoxTaskSolver.java
@@ -1,73 +1,45 @@
 package lepse.BoxTask;
 
 import java.io.File;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class BoxTaskSolver {
 
     public static void solve() {
+        ArrayList<Tile> tiles = new ArrayList<>();
+
         String filePath = "src/main/java/lepse/BoxTask/input.txt";
-        var dimsMap = new HashMap<Integer, Integer>();
-        int[][] values = new int[6][2];
-        var pairs = new int[6];
 
         try (var fileScanner = new Scanner(new File(filePath))) {
             for (int i = 0; i < 6; i++) {
                 int dim1 = fileScanner.nextInt();
-                values[i][0] = dim1;
-                dimsMap.put(dim1, 0);
-
                 int dim2 = fileScanner.nextInt();
-                values[i][1] = dim2;
-                dimsMap.put(dim2, 0);
-            }
 
-            if (dimsMap.keySet().size() > 3) {
-                System.err.println("Невозможно");
-                return;
-            }
-
-            for (int i = 0; i < 6; i++) {
-                for (int j = 0; j < 6; j++) {
-                    if (isPair(i, j, values)) {
-                        if ((pairs[i] < 2) && (pairs[j] < 2) && (pairs[i] == pairs[j])) {
-                            pairs[i]++;
-                            pairs[j]++;
-                            dimsMap.put(values[i][0], dimsMap.get(values[i][0]) + 1);
-                            dimsMap.put(values[i][1], dimsMap.get(values[i][1]) + 1);
-                        }
-                    }
+                if (dim1 < dim2) {
+                    tiles.add(new Tile(dim1, dim2));
+                } else {
+                    tiles.add(new Tile(dim2, dim1));
                 }
             }
 
-            for (var pair: pairs) {
-                if (pair != 2) {
-                    System.err.println("Невозможно");
-                    return;
-                }
-            }
+            tiles.sort(Comparator.comparingInt(Tile::getHeight).thenComparingInt(Tile::getWidth));
 
-            for (var entry : dimsMap.entrySet()) {
-                if (entry.getValue() < 4) {
-                    System.err.println("Невозможно");
-                    return;
-                }
-            }
+            boolean isBoxPossible = tiles.get(0).getHeight() == tiles.get(1).getHeight() &&
+                    tiles.get(1).getHeight() == tiles.get(2).getHeight() &&
+                    tiles.get(2).getHeight() == tiles.get(3).getHeight() &&
+                    tiles.get(0).getWidth() == tiles.get(1).getWidth() &&
+                    tiles.get(1).getWidth() == tiles.get(4).getHeight() &&
+                    tiles.get(4).getHeight() == tiles.get(5).getHeight() &&
+                    tiles.get(2).getWidth() == tiles.get(3).getWidth() &&
+                    tiles.get(3).getWidth() == tiles.get(4).getWidth() &&
+                    tiles.get(4).getWidth() == tiles.get(5).getWidth();
 
-            System.out.println("Возможно");
+            System.out.println(isBoxPossible ? "Возможно" : "Невозможно");
 
-        } catch (Exception e) {
+        } catch (Exception ex) {
             System.err.println("Невозможно");
         }
-    }
-
-    private static boolean isPair(int i, int j, int[][] values) {
-        if ((i != j)
-                && ((values[i][0] == values[j][0] && values[i][1] == values[j][1])
-                || (values[i][0] == values[j][1] && values[j][0] == values[i][1]))) {
-            return true;
-        }
-        return false;
     }
 }
---
+++ src/main/java/lepse/BoxTask/Tile.java
@@ -0,0 +1,19 @@
+package lepse.BoxTask;
+
+public class Tile {
+    private final int height;
+    private final int width;
+
+    public Tile(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
